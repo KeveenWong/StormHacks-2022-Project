5b45d7b77415b0b8ec4f13dfa7b3f4fa6144d81a
tanmay <tanmaythakral285@gmail.com>
2022-02-20 00:51:56 -0800

    some progress on frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
 import BodyComponent from './components/BodyComponent';
 import Articles from './components/Articles';
 import { useEffect , useState } from 'react';
+import axios from 'axios';
+
+
+function createArticle(prop) {
+  return <Articles 
+    title={prop.title}
+    img = {prop.img}
+    img_caption = {prop.img_caption}
+    body = {prop.body} />
+}
 function App() {
 
   const [data, setData] = useState(null);
+  const [count, setCount] = useState(0);
+
 
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data));
   }, []);
+
+
+  useEffect(() => {
+    fetch("/streak")
+      .then((res) => setCount(parseInt(res)))
+      .then(() => console.log(count));
+  }, []);
+
 
   return (
     <div className="App">
     <BodyComponent/>
-    <Articles
-
-    name = "king" 
-    article = "king" 
-    source = "king"
-
-    />
-    <p>{data}</p>
+    <h2> Streak Count : {count}</h2>
+    {!data ? "" : data.map(createArticle)}
     </div>
   );
 }
--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,12 +1,13 @@
 import react from "react";
-
+import Typography from '@mui/material/Typography';
 function Articles(props){
     return(
         <>
-        <h1>{props.name}</h1>
-        <br></br>
-        <h2>{props.article}</h2>
-        <h3>{props.source}</h3>
+        <Typography variant="h2" component="h2" color="white" gutterBottom ={true} align = "center" paddingLeft="2.5vw" paddingRight="2.5vw">{props.title}</Typography>;
+        <img src = {props.img} className="center"/>
+        <Typography variant="h4" component="h2" color="white" gutterBottom ={true} align = "center">{props.img_caption}</Typography>;
+        <Typography variant="h5" component="h2" color="white" gutterBottom ={true} align = "center" paddingLeft="2.5vw" paddingRight="2.5vw">{props.body}</Typography>;
+        <button>Read!</button>
         </>
     );
 }
