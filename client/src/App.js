072d0d75a554c11305536f37894d4a0826dbcd50
tanmay <tanmaythakral285@gmail.com>
2022-02-19 22:48:32 -0800

    backend added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import BodyComponent from './components/BodyComponent';
 import Articles from './components/Articles';
+import { useEffect , useState } from 'react';
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
+    <BodyComponent/>
     <Articles
 
     name = "king" 
     article = "king" 
     source = "king"
-    
+
     />
-    <Articles/>
-    <BodyComponent/>
+    <p>{data}</p>
     </div>
   );
 }
